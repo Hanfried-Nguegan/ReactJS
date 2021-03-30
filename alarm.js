c0408c13ef78cda88d510d809ee45bbec5238bae
Hanfried-Nguegan <80390250+Hanfried-Nguegan@users.noreply.github.com>
2021-03-30 23:06:45 +0530

    Add files via upload

--- alarm.js
+++ alarm.js
@@ -1,34 +1,56 @@
-import React from "react";
+import React, { useState } from "react";
 import { Switch } from "antd";
-export const alarms = [
-  {
-    id: 1,
-    name: "Alarm 1",
-    togggle: <Switch />,
-  },
-  {
-    id: 2,
-    name: "Alarm 2",
-    togggle: <Switch />,
-  },
-  {
-    id: 3,
-    name: "Alarm 3",
-    togggle: <Switch />,
-  },
-  {
-    id: 4,
-    name: "Alarm 4",
-    togggle: <Switch />,
-  },
-  {
-    id: 5,
-    name: "Alarm 5",
-    togggle: <Switch />,
-  },
-  {
-    id: 6,
-    name: "Alarm 6",
-    togggle: <Switch />,
-  },
-];
+import { IoIosAperture } from "react-icons/io";
+
+export default function DisplayAlarms(props) {
+  const { alarmState } = props;
+
+  const alarms = [
+    {
+      id: 1,
+      name: "Alarm 1",
+      togggle: <Switch checked={alarmState} />,
+    },
+    {
+      id: 2,
+      name: "Alarm 2",
+      togggle: <Switch checked={alarmState} />,
+    },
+    {
+      id: 3,
+      name: "Alarm 3",
+      togggle: <Switch checked={alarmState} />,
+    },
+    {
+      id: 4,
+      name: "Alarm 4",
+      togggle: <Switch checked={alarmState} />,
+    },
+    {
+      id: 5,
+      name: "Alarm 5",
+      togggle: <Switch checked={alarmState} />,
+    },
+    {
+      id: 6,
+      name: "Alarm 6",
+      togggle: <Switch checked={alarmState} />,
+    },
+  ];
+  return (
+    <div>
+      {alarms.map((alarm) => {
+        const { id, name, togggle } = alarm;
+        return (
+          <article key={id}>
+            <h4 className="alarm-name">
+              <IoIosAperture />
+              <span>{name}</span>
+              <span className="toggle">{togggle}</span>
+            </h4>
+          </article>
+        );
+      })}
+    </div>
+  );
+}
