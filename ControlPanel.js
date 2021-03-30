870286a6bf58c245b975069e1bd63517cccce4e5
Hanfried-Nguegan <80390250+Hanfried-Nguegan@users.noreply.github.com>
2021-03-30 23:06:03 +0530

    Add files via upload

--- ControlPanel.js
+++ ControlPanel.js
@@ -1,10 +1,8 @@
-import React, { useState } from "react";
-import { alarms } from "./alarm";
-import { IoIosAperture } from "react-icons/io";
+import React, { useState, useEffect } from "react";
+import DisplayAlarms from "./alarm";
 import { Switch } from "antd";
 
 function ControlPanel() {
-  const [isalarms, setisAlarms] = useState(alarms);
   const [toggle, setToggle] = useState(false);
   const [masterToggle, setMasterToggle] = useState(false);
   const masterToggler = () => {
@@ -14,28 +12,25 @@ function ControlPanel() {
   const toggler = () => {
     toggle ? setToggle(false) : setToggle(true);
   };
+
+  const turnOffAlarms = () => {
+    masterToggle == false && setToggle(false);
+  };
+
   return (
     <div className="controlpanel">
       <div className="alarm-toggles ">
         <div>
           <h4 className="master-control">
-            Master Control <Switch className="float-right pl-5" />
+            Master Control{" "}
+            <Switch
+              checked={masterToggle}
+              onClick={turnOffAlarms}
+              className="float-right pl-5"
+            />
           </h4>
+          <DisplayAlarms alarmState={toggle} />
         </div>
-        {isalarms.map((isalarm) => {
-          const { id, name, togggle } = isalarm;
-          return (
-            <article key={id}>
-              <h4 className="alarm-name">
-                <IoIosAperture />
-                <span>{name}</span>
-                <span className="toggle" onClick={toggler}>
-                  {togggle}
-                </span>
-              </h4>
-            </article>
-          );
-        })}
       </div>
       <div className="alarm-devices">Alarm Devices</div>
     </div>
