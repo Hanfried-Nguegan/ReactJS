64b8b7090d9ecc3e388b8baf81fa405bf7303822
Hanfried-Nguegan <80390250+Hanfried-Nguegan@users.noreply.github.com>
2021-04-01 22:09:14 +0530

    Add files via upload

--- ControlPanel.js
+++ ControlPanel.js
@@ -1,22 +1,40 @@
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 import DisplayAlarms from "./alarm";
 import { Switch } from "antd";
 
-function ControlPanel() {
+function ControlPanel({
+  setToggleAlarm1,
+  setToggleAlarm2,
+  setToggleAlarm3,
+  setToggleAlarm4,
+  setToggleAlarm5,
+  setToggleAlarm6,
+}) {
   const [toggle, setToggle] = useState(false);
   const [masterToggle, setMasterToggle] = useState(false);
+
   const masterToggler = () => {
     masterToggle ? setMasterToggle(false) : masterToggle(true);
   };
 
   const toggler = () => {
     toggle ? setToggle(false) : setToggle(true);
+    if (toggle === true) {
+      setToggleAlarm1(true) &&
+        setToggleAlarm2(true) &&
+        setToggleAlarm3(true) &&
+        setToggleAlarm4(true) &&
+        setToggleAlarm5(true) &&
+        setToggleAlarm6(true);
+    } else {
+      setToggleAlarm1(false) &&
+        setToggleAlarm2(false) &&
+        setToggleAlarm3(false) &&
+        setToggleAlarm4(false) &&
+        setToggleAlarm5(false) &&
+        setToggleAlarm6(false);
+    }
   };
-
-  const turnOffAlarms = () => {
-    masterToggle == false && setToggle(false);
-  };
-
   return (
     <div className="controlpanel">
       <div className="alarm-toggles ">
@@ -24,12 +42,12 @@ function ControlPanel() {
           <h4 className="master-control">
             Master Control{" "}
             <Switch
-              checked={masterToggle}
-              onClick={turnOffAlarms}
+              checked={toggle}
+              onClick={toggler}
               className="float-right pl-5"
             />
           </h4>
-          <DisplayAlarms alarmState={toggle} />
+          <DisplayAlarms toggle={toggle} />
         </div>
       </div>
       <div className="alarm-devices">Alarm Devices</div>
