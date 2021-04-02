8be2309f11e110707098c7dbfc79a6ca5f9a6842
Hanfried-Nguegan <80390250+Hanfried-Nguegan@users.noreply.github.com>
2021-04-02 20:32:26 +0530

    Add files via upload

--- ControlPanel.js
+++ ControlPanel.js
@@ -1,38 +1,13 @@
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 import DisplayAlarms from "./alarm";
 import { Switch } from "antd";
 
-function ControlPanel(props) {
-  const {
-    setToggleAlarms1,
-    setToggleAlarms2,
-    setToggleAlarms3,
-    setToggleAlarms4,
-    setToggleAlarms5,
-    setToggleAlarms6,
-  } = props;
+function ControlPanel() {
   const [toggle, setToggle] = useState(false);
 
   const toggler = () => {
     toggle ? setToggle(false) : setToggle(true);
   };
-  useEffect(() => {
-    if (toggle === true) {
-      setToggleAlarms1(true) &&
-        setToggleAlarms2(true) &&
-        setToggleAlarms3(true) &&
-        setToggleAlarms4(true) &&
-        setToggleAlarms5(true) &&
-        setToggleAlarms6(true);
-    } else {
-      setToggleAlarms1(false) &&
-        setToggleAlarms2(false) &&
-        setToggleAlarms3(false) &&
-        setToggleAlarms4(false) &&
-        setToggleAlarms5(false) &&
-        setToggleAlarms6(false);
-    }
-  }, [toggle]);
 
   return (
     <div className="controlpanel">
